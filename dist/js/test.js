16e35f0c1fc6586206fb9cbe0623e2df94856bf2
skycherry <770754765@qq.com>
2021-06-01 18:32:23 +0800

    考试

---
+++ dist/js/test.js
@@ -0,0 +1,16 @@
+$(function() {
+    var testPage = new Vue({
+        el: '#testPage',
+        data: function () {
+            return {
+                radio: '1',
+                checked: true,
+            }
+        },
+        mounted: function () {
+        },
+        methods: {
+
+        }
+    });
+})
